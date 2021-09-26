f8b1f975920d0522079dcc0261bfb0434ccbe68f
Paulo de Tarso <zangarmarsh@gmail.com>
2021-09-26 13:46:10 -0300

    add save game/export function at end of script

--- cookieclicker.py
+++ cookieclicker.py
@@ -6,24 +6,27 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-#url = driver.command_executor._url #http://127.0.0.1:62555
-#session_id = driver.session_id #824a355500ca27a95801bed9ecbcd79d
 
-for i in range(0, 1000):
+def save_game():
+    options = driver.find_element_by_css_selector("#prefsButton")
+    options.click()
+    export_save = driver.find_elements_by_css_selector("#menu .subsection .listing .option")
+    export_save[1].click()
+
+    save_game_id = driver.find_element_by_css_selector("#textareaPrompt")
+    with open("save_game.txt", 'w') as file:
+        file.write(save_game_id.text)
+
+    print(f"Game Save exported successfully.\n Id: {save_game_id.text}")
+
+
+for i in range(0, 15):
     cookie = driver.find_element_by_css_selector("#bigCookie")
     cookie.click()
-
 
 total_cookies = driver.find_element_by_css_selector("#cookies")
 print(total_cookies.text)
 
-#save = driver.find_element_by_css_selector()
-
-# TODO Save progress
+save_game()
 
 # TODO Load progress
-
-
-
-
-
